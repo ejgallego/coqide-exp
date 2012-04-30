ejgallego/coqide-exp

commit 68061b21540b9aaf949ecfda2f9b49cba3a4ea0e

Coqide MacOS integration refresh
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15254 85f007b7-540e-0410-9357-904b9bb8a0f7

coqide.ml
@@ -2229,13 +2229,13 @@ let main files =
                    session_notebook#current_term.script#as_view
                    ~sgn:GtkText.View.S.paste_clipboard
              with _ -> prerr_endline "EMIT PASTE FAILED") ~stock:`PASTE;
-      GAction.add_action "Find" ~label:"_Find" ~stock:`FIND ~accel:"<Ctrl>F"
+      GAction.add_action "Find" ~stock:`FIND
         ~callback:(fun _ -> session_notebook#current_term.finder#show_find ());
       GAction.add_action "Find Next" ~label:"Find _Next" ~stock:`GO_DOWN ~accel:"F3"
         ~callback:(fun _ -> session_notebook#current_term.finder#find_forward ());
       GAction.add_action "Find Previous" ~label:"Find _Previous" ~stock:`GO_UP ~accel:"<Shift>F3"
         ~callback:(fun _ -> session_notebook#current_term.finder#find_backward ());
-      GAction.add_action "Replace" ~label:"_Replace" ~stock:`FIND_AND_REPLACE ~accel:"<Ctrl>R"
+      GAction.add_action "Replace" ~stock:`FIND_AND_REPLACE
         ~callback:(fun _ -> session_notebook#current_term.finder#show_replace ());
      GAction.add_action "Close Find" ~accel:"Escape"
         ~callback:(fun _ -> session_notebook#current_term.finder#hide ());
@@ -2258,7 +2258,7 @@ let main files =
 	  try configure ~apply:update_notebook_pos ()
 	  with _ -> flash_info "Cannot save preferences"
 	end;
-	reset_revert_timer ()) ~stock:`PREFERENCES;
+	reset_revert_timer ()) ~accel:"<Ctrl>," ~stock:`PREFERENCES;
       (* GAction.add_action "Save preferences" ~label:"_Save preferences" ~callback:(fun _ -> save_pref ()); *) ];
     GAction.add_actions view_actions [
       GAction.add_action "View" ~label:"_View";

ide_mac_stubs.c
@@ -8,7 +8,7 @@
 #include <lablgtk2/wrappers.h>
 #include <lablgtk2/ml_glib.h>
 #include <lablgtk2/ml_gobject.h>
-#include <igemacintegration/gtkosxapplication.h>
+#include <gtkmacintegration/gtkosxapplication.h>
 
 GtkOSXApplication *theApp;
 value open_file_fun, forbid_quit_fun, themenubar, pref_item, about_item;
@@ -76,10 +76,10 @@ CAMLprim value caml_gtk_mac_ready(value menubar, value prefs, value about)
   caml_register_generational_global_root(&about_item);
   /*  gtk_accel_map_foreach(NULL, osx_accel_map_foreach_lcb);*/
   gtk_osxapplication_set_menu_bar(theApp,check_cast(GTK_MENU_SHELL,themenubar));
-  about_grp = gtk_osxapplication_add_app_menu_group(theApp);
-  pref_grp = gtk_osxapplication_add_app_menu_group(theApp);
-  gtk_osxapplication_add_app_menu_item(theApp,about_grp,check_cast(GTK_MENU_ITEM,about_item));
-  gtk_osxapplication_add_app_menu_item(theApp,pref_grp,check_cast(GTK_MENU_ITEM,pref_item));
+  gtk_osxapplication_insert_app_menu_item(theApp,check_cast(GTK_WIDGET,about_item),1);
+  gtk_osxapplication_insert_app_menu_item(theApp,gtk_separator_menu_item_new(),2);
+  gtk_osxapplication_insert_app_menu_item(theApp,check_cast(GTK_WIDGET,pref_item),3);
+  gtk_osxapplication_insert_app_menu_item(theApp,gtk_separator_menu_item_new(),4);
   gtk_osxapplication_ready(theApp);
   CAMLreturn(Val_unit);
 }

commit 1ff296c1215598e10a42f8a60887627a79b645f7

Fixed an accelerator parsing error in CoqIDE introduced in r15254
git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15258 85f007b7-540e-0410-9357-904b9bb8a0f7

coqide.ml
@@ -2258,7 +2258,7 @@ let main files =
 	  try configure ~apply:update_notebook_pos ()
 	  with _ -> flash_info "Cannot save preferences"
 	end;
-	reset_revert_timer ()) ~accel:"<Ctrl>," ~stock:`PREFERENCES;
+	reset_revert_timer ()) ~accel:"<Ctrl>comma" ~stock:`PREFERENCES;
       (* GAction.add_action "Save preferences" ~label:"_Save preferences" ~callback:(fun _ -> save_pref ()); *) ];
     GAction.add_actions view_actions [
       GAction.add_action "View" ~label:"_View";